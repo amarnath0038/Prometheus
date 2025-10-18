2961801b20b8b20df80a810db2f2c703b6996d08
amarnath0038 <amarnathreddy841@gmail.com>
2025-10-18 12:07:36 +0530

    Added metrics endpoint

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import client from "prom-client";
+import { middleware } from "./middleware.js";
+import { metricsMiddleware } from "./metrics/index.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(middleware);
+app.use(metricsMiddleware);
+
+
+app.get("/user", async (req, res) => {
+    await new Promise((x) => setTimeout(x, Math.random() * 2500))
+    res.send({
+        name: "Amar",
+        age: 20
+    });
+});
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1
+    });
+});
+
+app.get("/metrics", async (req, res) => {
+    const metrics = await client.register.metrics();
+    res.set('Content-Type', client.register.contentType);
+    res.end(metrics);
+})
