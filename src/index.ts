d7685a2928993ccbfa56f96f1f2b2a289330d1f0
amarnath0038 <amarnathreddy841@gmail.com>
2025-10-18 12:09:35 +0530

    Exposed the port

--- src/index.ts
+++ src/index.ts
@@ -30,4 +30,8 @@ app.get("/metrics", async (req, res) => {
     const metrics = await client.register.metrics();
     res.set('Content-Type', client.register.contentType);
     res.end(metrics);
-})
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
